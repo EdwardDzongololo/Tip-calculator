b1ee3be273cb132e7d1552d6e786e88b68b37605
Edward Dzongololo <98263200+EdwardDzongololo@users.noreply.github.com>
2023-08-21 01:34:35 +0300

    .

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
-const firstTip=document.getElementById("tip-1");
+// cons=document.getElementById("tip-1");
 const price=document.getElementById("price");
 const total=document.getElementById("total")
 const NumberOfPeople=document.getElementById("number-of-people");
-
-
+const btn=document.querySelectorAll(".tip")
 const personal=document.getElementById("person")
-firstTip.addEventListener("click", function(){
-    const tip1=(Number(price.value)*5/100)
-    total.innerHTML="$"+tip1
-const perPerson= tip1/(Number(NumberOfPeople.value))
-personal.innerHTML="$"+perPerson
-})
+const warning=document.querySelector(".warning")
+const customBtn=document.querySelector(".clear-input-button")
+const custom=document.getElementById("custom")
 
 
+function calculateTip(){
+    btn.forEach(function(btn){
+    btn.addEventListener("click", function(){
+       
+        
+        let tipPercentage=(btn.getAttribute('data-value'))
+        let tip=(Number(price.value)*tipPercentage/100)
+            total.innerHTML="$"+tip
+           people=(Number(NumberOfPeople.value))
+           if (people===0||""){
+            warning.style.display="flex";
+        personal.innerHTML= "$"+"---"
+           }else
+           {const perPerson= tip/people
+           personal.innerHTML="$"+perPerson}
+        })
+      
+    })
+    }
+    
+    
+    calculateTip()
+   {
+        const resetBtn=document.querySelector(".reset")
+        resetBtn.addEventListener("click",function(){
+            document.getElementById("form").reset();
+            document.getElementById("form2").reset()
+        })
+    }
+       
 
+// let inputValue=(Number(custom.value))
+// //  (inputValue>0||!""){
+//     function customEl(){
+//         customBtn.addEventListener("click",function(){
+//             let tip=(Number(price.value)*inputValue/100)
+//             total.innerHTML="$"+tip
+//            people=(Number(NumberOfPeople.value))
+//            {const perPerson= tip/people
+//            personal.innerHTML="$"+perPerson}
+//         })
+    
+//     }
 
-// const price=document.getElementById("price").value 
-// console.log(price)
+//      function customEl()
+   
